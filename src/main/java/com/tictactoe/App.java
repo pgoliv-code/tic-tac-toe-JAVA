fea8481725d4c1aa21a2be7699e30be0e93c066d
Priscila Oliveira <pguillen.oliv@gmail.com>
2025-03-13 13:01:25 +0100

    first commit

---
+++ src/main/java/com/tictactoe/App.java
@@ -0,0 +1,13 @@
+package com.tictactoe;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Juguemos al tic tac toe");
+        System.out.println( "El tablero se compone de fila 0 - 1 y 2 y de columna 0 - 1 y 2");
+
+        
+
+    }
+}
